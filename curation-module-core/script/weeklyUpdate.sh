77cd23a4f6a1526aa95916e966c726dcdf4f8878
coy123 <canyil@gmail.com>
2020-11-04 13:23:14 +0100

    update weeklyUpdate.sh

--- curation-module-core/script/weeklyUpdate.sh
+++ curation-module-core/script/weeklyUpdate.sh
@@ -7,13 +7,13 @@ DATA_DIR=$WORK_DIR/data
 CONF_DIR=$WORK_DIR/conf
 
 HARVESTER_URL=https://vlo.clarin.eu/resultsets
-#RESULTSETS="clarin.tar.bz2 others.tar.bz2"
+
 RESULTSETS="clarin.tar.bz2 others.tar.bz2 europeana.tar.bz2"
-#RESULTSETS="clarin.tar.bz2"
+
 CMDI_PATH=results/cmdi
 
 LOG4J=-Dlog4j.configuration=file:$CONF_DIR/log4j.properties
-VM_ARGS="-Xms4G -Xmx8G -XX:+UseG1GC -XX:-UseParallelGC -XX:+UseStringDeduplication -XX:MaxHeapFreeRatio=20 -XX:MinHeapFreeRatio=10 -XX:GCTimeRatio=20"
+VM_ARGS="-Xms4G -Xmx12G -XX:+UseG1GC -XX:-UseParallelGC -XX:+UseStringDeduplication -XX:MaxHeapFreeRatio=20 -XX:MinHeapFreeRatio=10 -XX:GCTimeRatio=20"
 
 XSD_CACHE=$WORK_DIR/xsd_cache
 
@@ -21,36 +21,40 @@ XSD_CACHE=$WORK_DIR/xsd_cache
 set -e
 
 #delete old data in case not done before
-#echo "delete old data in case not done before..."
-#if [ -e $DATA_DIR ]; then
-#	chmod -R a+w $DATA_DIR
-#	rm -rf $DATA_DIR
-#fi
+echo "delete old data in case not done before..."
+if [ -e $DATA_DIR ]; then
+	chmod -R a+w $DATA_DIR
+	rm -rf $DATA_DIR
+fi
 
 # create new data directory
-#mkdir -p $DATA_DIR/clarin
-#mkdir $DATA_DIR/europeana
+mkdir -p $DATA_DIR/clarin
+mkdir $DATA_DIR/europeana
 
 #get harvested collections
-#for RESULTSET in $RESULTSETS; do
-#	if [ "$RESULTSET" = "europeana.tar.bz2" ]; then
-#		cd $DATA_DIR/europeana
-#	else
-#		cd $DATA_DIR/clarin
-#	fi
-#	#download tar
-#	wget $HARVESTER_URL/$RESULTSET
-#
-#	echo "unpacking $RESULTSET..."
-#	#unpack CMDI 1.2 files
-#	tar -xjf $RESULTSET $CMDI_PATH
-#
-#	#delete tar
-#	rm $RESULTSET
-#done
+for RESULTSET in $RESULTSETS; do
+	if [ "$RESULTSET" = "europeana.tar.bz2" ]; then
+		cd $DATA_DIR/europeana
+	else
+		cd $DATA_DIR/clarin
+	fi
+	#download tar
+	wget $HARVESTER_URL/$RESULTSET
+
+	echo "unpacking $RESULTSET..."
+	#unpack CMDI 1.2 files
+	tar -xjf $RESULTSET $CMDI_PATH
+
+	#delete tar
+	rm $RESULTSET
+done
+
+# protecting files
+cd $WORK_DIR
+chmod -R a-w $DATA_DIR
 
 echo "generating new reports, downloading necessary profiles..."
-java $VM_ARGS -Dprojectname=curate $LOG4J -jar $BIN_DIR/curation-module-core-4.0-jar-with-dependencies.jar -config $CONF_DIR/config.properties -r -path $DATA_DIR/clarin/$CMDI_PATH $DATA_DIR/europeana/$CMDI_PATH
+java $VM_ARGS -Dprojectname=curate $LOG4J -jar $BIN_DIR/curate.jar -config $CONF_DIR/config.properties -r -path $DATA_DIR/clarin/$CMDI_PATH $DATA_DIR/europeana/$CMDI_PATH
 echo "report generation finished. creating value maps..."
 
 # create value maps
@@ -66,6 +70,6 @@ done
 echo "Finished!"
 ELAPSED_TIME=$(($SECONDS - $START_TIME))
 echo "Elapsed time: $(($ELAPSED_TIME/60)) min"
-echo "please restart curate webapp with 'docker-manage -e clarin-curate -v'"
+echo "Please restart curate webapp..."
 #restart curate webapp
-#docker-manage -e clarin-curate -v
+#docker compose clarin TODO need to check
